2392f8380a8645a11d7d47fc4bb57275a5501ac4
Max <maxjoehnk@gmail.com>
2018-04-10 17:58:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RGB LED Artnet Host
+
+Setup Script for a Raspberry Pi connected to 8 RGB LEDs, controllable via PWM.
---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -e
+
+echo [1/5] Installing Dependencies...
+sudo apt-get update && \
+    sudo apt-get install -y --no-install-recommends \
+        git \
+        build-essentials \
+        pigpio
+echo Done
+
+echo [2/5] Installing Node.JS...
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo Done
+
+echo [3/5] Installing gpio-artnet-node...
+git clone https://github.com/maxjoehnk/gpio-artnet-node.git
+cd gpio-artnet-node
+npm i
+echo Done
+
+echo [4/5] Setting up Systemd Service...
+cp gpio-artnet-node.service /lib/systemd/system/
+echo Done
+
+echo [5/5] Activating Service...
+sudo systemctl enable gpio-artnet-node
+sudo systemctl start gpio-artnet-node
+echo Done
